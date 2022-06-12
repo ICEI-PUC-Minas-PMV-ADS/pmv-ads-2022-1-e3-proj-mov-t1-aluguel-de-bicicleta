a445eeba330e3636116764213110409c484c59d5
suepuc <1337804@sga.pucminas.br>
2022-06-12 16:23:43 -0300

    Carry on w/ DateTimePicker trials

--- bikeRentalMobile/screens/reservations/addReservation.tsx
+++ bikeRentalMobile/screens/reservations/addReservation.tsx
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from "react";
 import { RootStackScreenProps } from "../../types";
 import Colors from "../../constants/Colors";
 import PageHeader from "../../common/pageHeader";
-import { ListRenderItem, StyleSheet, View, FlatList } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, SafeAreaView, Text, ListRenderItem, StyleSheet, View, FlatList } from "react-native";
 import { StyledInput, StyledLabel } from "../../common/styled";
 import styled from "styled-components/native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useDispatch } from "react-redux";
 import { createReservation } from "../../services/api";
 import {
-  Button,
   Paragraph,
   Dialog,
   Portal,
@@ -33,8 +31,32 @@ function AddReservation({navigation,}: RootStackScreenProps<"AddReservation">): 
   );
   const [show, setShow] = useState(false);
   const [data, setData] = useState(moment(new Date()).format("DD/MM/YYYY"));
+  const [datePicker, setDatePicker] = useState(false);
+  const [timePicker, setTimePicker] = useState(false);
+  const [time, setTime] = useState(false);
+  const [date, setDate] = useState(new Date());
+  
   const [allBikes] = useState<IBike[]>([]);
    
+// function showDatePicker(){
+//   setDatePicker(true);
+// }
+
+// function showTimePicker(){
+//   setTimePicker(true);
+// }
+
+// function onDateSelected(event, value) {
+//   setDate(value);
+//   setDatePicker(false);
+// };
+
+// function onTimeSelected(event, value) {
+//   setTime(value);
+//   setTimePicker(false);
+// };
+
+
   const renderBikes: ListRenderItem<IBike> = ({ item }) => (
     <>
       <List.Item
@@ -70,100 +92,174 @@ function AddReservation({navigation,}: RootStackScreenProps<"AddReservation">): 
     }
   };
 
-  return (
+  // return (
     
-    <SafeAreaView style={{ flex: 1 }}>
-      <StyledCreateReservation>
-      <PageHeader
-        pageName="New Reservation"
-        navigation={navigation}
-        style={styles.pageHeaderX}
-      />
-      <FlatList style
-        data={allBikes}
-        renderItem={renderBikes}
-        keyExtractor={(bike: { _id: any; }) => bike._id}
-      />
-      <View style={styles.inputBox}>
-        {/* <View style={styles.containerInputs}>
-          <StyledLabel>Bike ID:</StyledLabel>
-          <StyledInput
-            style={styles.sombraChique}
-            textContentType="name"
-            value={newReservation.bikeId}
-            onChangeText={(value) =>
-              setNewReservation({ ...newReservation, bikeId: value })
-            }
-          />
-        </View> */}
-        <View style={styles.containerInputs}>
-          <StyledLabel>To:</StyledLabel>
-          <StyledInput
-            style={styles.sombraChique}
-            mode={"date"}
-            value={16546404247}
-            onPressIn={(value) =>
-              setNewReservation({ ...newReservation, startTimestamp: value })
-            }
-          />
-        </View>
-        <View style={styles.containerInputs}>
-          <StyledLabel>From:</StyledLabel>
-          <StyledInput
-            style={styles.sombraChique}
-            mode={"date"}
-            value={1654640424744}
-            onPressIn={(value) =>
-              setNewReservation({ ...newReservation, endTimestamp: value })
-            }
-          />
-        </View>
-        <View style={styles.containerButtons}>
-          <OptionListButton
-            onPress={onSaveReservation}
-            style={{ elevation: 10 }}
-          >
-            <ButtonText>Save</ButtonText>
-            <MaterialIcons size={25} name="save" color="white" />
-          </OptionListButton>
-          <OptionListButton
-            style={{ backgroundColor: "gray", elevation: 10 }}
-            onPress={() => navigation.goBack()}
-          >
-            <ButtonText>Cancel</ButtonText>
-          </OptionListButton>
-        </View>
-      </View>
+  //   <SafeAreaView style={{ flex: 1 }}>
+  //     <StyledCreateReservation>
+  //     <PageHeader
+  //       pageName="New Reservation"
+  //       navigation={navigation}
+  //       style={styles.pageHeaderX}
+  //     />
+  //     <FlatList style
+  //       data={allBikes}
+  //       renderItem={renderBikes}
+  //       keyExtractor={(bike: { _id: any; }) => bike._id}
+  //     />
+  //     <View style={styles.inputBox}>
+  //       {/* <View style={styles.containerInputs}>
+  //         <StyledLabel>Bike ID:</StyledLabel>
+  //         <StyledInput
+  //           style={styles.sombraChique}
+  //           textContentType="name"
+  //           value={newReservation.bikeId}
+  //           onChangeText={(value) =>
+  //             setNewReservation({ ...newReservation, bikeId: value })
+  //           }
+  //         />
+  //       </View> */}
+  //       <View style={styles.containerInputs}>
+  //         <StyledLabel>To:</StyledLabel>
+  //         <StyledInput
+  //           style={styles.sombraChique}
+  //           mode={"date"}
+  //           value={16546404247}
+  //           onPressIn={(value) =>
+  //             setNewReservation({ ...newReservation, startTimestamp: value })
+  //           }
+  //         />
+  //       </View>
+  //       <View style={styles.containerInputs}>
+  //         <StyledLabel>From:</StyledLabel>
+  //         <StyledInput
+  //           style={styles.sombraChique}
+  //           mode={"date"}
+  //           value={1654640424744}
+  //           onPressIn={(value) =>
+  //             setNewReservation({ ...newReservation, endTimestamp: value })
+  //           }
+  //         />
+  //       </View>
+  //       <View style={styles.containerButtons}>
+  //         <OptionListButton
+  //           onPress={onSaveReservation}
+  //           style={{ elevation: 10 }}
+  //         >
+  //           <ButtonText>Save</ButtonText>
+  //           <MaterialIcons size={25} name="save" color="white" />
+  //         </OptionListButton>
+  //         <OptionListButton
+  //           style={{ backgroundColor: "gray", elevation: 10 }}
+  //           onPress={() => navigation.goBack()}
+  //         >
+  //           <ButtonText>Cancel</ButtonText>
+  //         </OptionListButton>
+  //       </View>
+  //     </View>
 
-      {/* <DateTimePicker
+      { show && (<DateTimePicker
             testID="dateTimePicker"
-            value={new Date(newReservation.startTimestamp)}
-            mode={'date'}
+            value= {(new Date (newReservation.startTimestamp))}
+            mode={"date"}
             is24Hour={true}
             display="default"
             onTouchCancel={() => setShow(false)}
-            onChange={(event: any, date: any) => {
+            onChange={(_event: any, value: number) => {
               setShow(false);
-              setData(moment(date).format('DD/MM/YYYY'));
-             setNewReservation({ ... newReservation, startTimeStamp: Date.parse(value)});
-              setNewReservation({...newReservation, startTimeStamp: Date.parse(moment(date).format('DD/MM/YYYY'))}),
+              //setData(moment(date).format('DD/MM/YYYY'));
+             //setNewReservation({ ... newReservation, startTimeStamp: Date.parse(value)});
+              setNewReservation({...newReservation, startTimestamp: value});
             }}
-          /> */}
+          />) }
 
-       {/* <TouchableOpacity onPress={() => setShow(true)}>
-          <Input
-            label="Data"
-            value={data}
-            left={<TextInput.Icon name="calendar" />}
-            editable={false}
-          />
-        </TouchableOpacity> */}
-      </StyledCreateReservation>
-    </SafeAreaView>
-  );
+  //      {/* <TouchableOpacity onPress={() => setShow(true)}>
+  //         <Input
+  //           label="Data"
+  //           value={data}
+  //           left={<TextInput.Icon name="calendar" />}
+  //           editable={false}
+  //         />
+  //       </TouchableOpacity> */}
+  //     </StyledCreateReservation>
+  //   </SafeAreaView>
+  // );
+
+  // return (
+  //   <SafeAreaView style={{ flex: 1 }}>
+  //     <View style={styleSheet.MainContainer}>
+ 
+  //       <Text style={styleSheet.text}>Date = {date.toDateString()}</Text>
+ 
+  //       <Text style={styleSheet.text}>Time = {time}</Text>
+ 
+  //       {datePicker && (
+  //         <DateTimePicker
+  //           value={date}
+  //           mode={'date'}
+  //           display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+  //           is24Hour={true}
+  //           onChange={onDateSelected}
+  //           style={styleSheet.datePicker}
+  //         />
+  //       )}
+ 
+  //       {timePicker && (
+  //         <DateTimePicker
+  //           value={time}
+  //           mode={'time'}
+  //           display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+  //           is24Hour={false}
+  //           onChange={onTimeSelected}
+  //           style={styleSheet.datePicker}
+  //         />
+  //       )}
+ 
+  //       {!datePicker && (
+  //         <View style={{ margin: 10 }}>
+  //           <Button title="Show Date Picker" color="green" onPress={showDatePicker} />
+  //         </View>
+  //       )}
+ 
+  //       {!timePicker && (
+  //         <View style={{ margin: 10 }}>
+  //           <Button title="Show Time Picker" color="green" onPress={showTimePicker} />
+  //         </View>
+  //       )}
+ 
+  //     </View>
+  //   </SafeAreaView>
+  // );
 }
 
 export default AddReservation;
+
+const styleSheet = StyleSheet.create({
+ 
+  MainContainer: {
+    flex: 1,
+    padding: 6,
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+ 
+  text: {
+    fontSize: 25,
+    color: 'red',
+    padding: 3,
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+ 
+  // Style for iOS ONLY...
+  datePicker: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    width: 320,
+    height: 260,
+    display: 'flex',
+  },
+ 
+});
 
 const StyledCreateReservation = styled.KeyboardAvoidingView`
   padding: ${defaultPadding}px;
@@ -222,4 +318,8 @@ const ButtonText = styled.Text`
   color: white;
   padding-right: 10px;
 `;
+
+function value(value: any) {
+  throw new Error("Function not implemented.");
+}
 
