03ef3b8b805125a06b6527bc6f186e9affcb72ec
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-06-16 19:26:29 -0300

    lint

--- bikeRentalMobile/screens/profile/profileScreen.tsx
+++ bikeRentalMobile/screens/profile/profileScreen.tsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
-import { RootStackScreenProps } from "../../types";
 import { useDispatch, useSelector } from "react-redux";
+import { FAB, List } from "react-native-paper";
+import { StyleSheet, FlatList, ListRenderItem } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { RootStackScreenProps } from "../../types";
 import Colors from "../../constants/Colors";
 import { defaultPadding } from "../../constants/Layout";
-import PageHeader from '../../common/pageHeader';
-import { FAB, List } from 'react-native-paper';
-import { StyleSheet, FlatList, ListRenderItem } from 'react-native';
+import PageHeader from "../../common/pageHeader";
 import { fetchUserReservations } from "../../services/api";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function Perfil({ navigation }: RootStackScreenProps<"Perfil">): JSX.Element {
-
   const { loggedUser } = useSelector(
     (state: { loggedUser: UserObject }) => state
   );
@@ -21,60 +20,57 @@ function Perfil({ navigation }: RootStackScreenProps<"Perfil">): JSX.Element {
   const handleGoToEditProfile = () => {
     navigation.navigate("EditProfile");
   };
-  
+
   useEffect(() => {
     setUser(user);
 
     if (user && user._id) {
       fetchUserReservations(user._id).then((response) => {
         console.log(response.data);
-        let data = response.data.filter(
-          x => new Date().getDate() >= new Date(x.startTimestamp).getDate()
-            && new Date().getDate() <= new Date(x.endTimestamp).getDate()
+        const data = response.data.filter(
+          (x) =>
+            new Date().getDate() >= new Date(x.startTimestamp).getDate() &&
+            new Date().getDate() <= new Date(x.endTimestamp).getDate()
         );
         setUserReservations(data);
       });
     }
-
   }, [loggedUser]);
 
   const renderItem: ListRenderItem<IReservation> = ({ item }) => (
-    <>
-      <List.Item
-        style={styles.listContainer}
-        title={item.bikeInfo.model}
-        descriptionNumberOfLines={3}
-        description={
-          `Location: ${item.bikeInfo.location}\n` +
-          `From: ${new Date(item.startTimestamp).toLocaleString('pt-br')}\n` +
-          `To: ${new Date(item.endTimestamp).toLocaleString('pt-br')}`
-        }
-        left={(props) => (
-          <List.Icon
-            {...props}
-            color="#f4bd5a"
-            icon="bike"
-          />
-        )}
-      />
-    </>
+    <List.Item
+      style={styles.listContainer}
+      title={item.bikeInfo.model}
+      descriptionNumberOfLines={3}
+      description={
+        `Location: ${item.bikeInfo.location}\n` +
+        `From: ${new Date(item.startTimestamp).toLocaleString("pt-br")}\n` +
+        `To: ${new Date(item.endTimestamp).toLocaleString("pt-br")}`
+      }
+      left={(props) => <List.Icon {...props} color="#f4bd5a" icon="bike" />}
+    />
   );
 
   return (
     <StyledSelectedUser>
-
       <PageHeader pageName="My Profile" navigation={navigation} />
 
-      <StyledUserName>{user.firstName} {user.lastName}</StyledUserName>
+      <StyledUserName>
+        {user.firstName} {user.lastName}
+      </StyledUserName>
 
       <StyledStrong>email</StyledStrong>
       <StyledUserProp>{user.email}</StyledUserProp>
 
       <StyledStrong>manager</StyledStrong>
-      <StyledUserProp>{user.isManager ? 'Yes' : 'No'}</StyledUserProp>
+      <StyledUserProp>{user.isManager ? "Yes" : "No"}</StyledUserProp>
 
       <BookingsTitleContainer>
-        <MaterialCommunityIcons name="calendar-check" size={18} color="#457B9D" />
+        <MaterialCommunityIcons
+          name="calendar-check"
+          size={18}
+          color="#457B9D"
+        />
         <StyledStrong> Current Bookings</StyledStrong>
       </BookingsTitleContainer>
 
@@ -84,12 +80,7 @@ function Perfil({ navigation }: RootStackScreenProps<"Perfil">): JSX.Element {
         keyExtractor={(reservation) => reservation._id}
       />
 
-      <FAB
-        style={styles.fab}
-        icon="pencil"
-        onPress={handleGoToEditProfile}
-      />
-
+      <FAB style={styles.fab} icon="pencil" onPress={handleGoToEditProfile} />
     </StyledSelectedUser>
   );
 }
@@ -145,11 +136,10 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   fab: {
-    position: 'absolute',
+    position: "absolute",
     margin: 16,
     left: 10,
     bottom: 10,
     backgroundColor: "white",
   },
 });
-
