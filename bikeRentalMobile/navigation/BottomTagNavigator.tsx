cfb5ecfd90d18bef297b1ab025574cfcf543c9cc
Renê Boaventura <reneboaventuraneto@gmail.com>
2022-06-18 18:37:25 -0300

    fix reservation list heading

--- bikeRentalMobile/navigation/BottomTagNavigator.tsx
+++ bikeRentalMobile/navigation/BottomTagNavigator.tsx
@@ -1,17 +1,11 @@
-import { FontAwesome } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { useNavigation } from "@react-navigation/native";
-import { Pressable } from "react-native";
 
 import Colors from "../constants/Colors";
 import useColorScheme from "../hooks/useColorScheme";
 
 import HomeScreen from "../screens/home/homeScreen";
-import AddReservation from "../screens/reservations/addReservation";
-import ReservationList from "../screens/reservations/reservationList";
-import TabTwoScreen from "../screens/TabTwoScreen";
 import { RootTabParamList } from "../types";
-import { HomeTabBarIcon, TabTwoBarIcon } from "./BottomTabBarIcons";
+import { HomeTabBarIcon } from "./BottomTabBarIcons";
 
 /**
  * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
@@ -38,36 +32,8 @@ function BottomTabNavigator() {
           tabBarIcon: HomeTabBarIcon,
         }}
       />
-      <BottomTab.Screen
-        name="ReservationList"
-        component={ReservationList}
-        options={() => ({
-          title: "Reservations",
-          tabBarIcon: TabTwoBarIcon,
-          headerRight: TabTwoHeader,
-        })}
-      />
     </BottomTab.Navigator>
   );
 }
 
-function TabTwoHeader() {
-  const colorScheme = useColorScheme();
-  const navigation = useNavigation();
-  return (
-    <Pressable
-      onPress={() => navigation.navigate("Modal")}
-      style={({ pressed }) => ({
-        opacity: pressed ? 0.5 : 1,
-      })}
-    >
-      <FontAwesome
-        name="info-circle"
-        size={25}
-        color={Colors[colorScheme].black}
-        style={{ marginRight: 15 }}
-      />
-    </Pressable>
-  );
-}
 export default BottomTabNavigator;
